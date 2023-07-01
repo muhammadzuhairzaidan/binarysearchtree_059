eb81264d231de6090c3241946e60c07da4354c04
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-07-01 20:25:32 +0700

    prosedur search

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -45,4 +45,16 @@ public:
 			parent->rightchild = newNode; //Make the right child of the parent pointto thenew node
 		}
 	}
+
+	void search(string element, Node*& parent, Node*& currentNode) { //This function searches the cuurentNode of the specified Node current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element)) {
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
 };
