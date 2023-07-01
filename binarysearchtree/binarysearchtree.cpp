b45920cff36d3b3bb0267330027a3b7883f3f21e
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-07-01 20:34:42 +0700

    prosedur preorder

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -69,4 +69,17 @@ public:
 			inorder(ptr->rightchild);
 		}
 	}
+
+
+	void preorder(Node* ptr) {
+		if (ROOT == NULL) {
+			cout << "Tree is emprty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
+	}
 };
