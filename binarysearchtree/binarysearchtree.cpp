a94894ec4803c583fb0986ba55a428eb68c5caee
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-07-01 20:48:32 +0700

    method post order

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -83,15 +83,15 @@ public:
 		}
 	}
 
-	void preorder(Node* ptr) {
+	void postorder(Node* ptr) { //Performs the postorder traversal of the tree
 		if (ROOT == NULL) {
 			cout << "Tree is emprty" << endl;
 			return;
 		}
 		if (ptr != NULL) {
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
 			cout << ptr->info << " ";
-			preorder(ptr->leftchild);
-			preorder(ptr->rightchild);
 		}
 	}
 };
