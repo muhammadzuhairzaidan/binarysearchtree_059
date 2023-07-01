ff9fc0aed658b6d47dc01ea23408d252adf6927e
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-07-01 20:51:18 +0700

    prosedur utama

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -95,3 +95,48 @@ public:
 		}
 	}
 };
+
+int main() {
+	BinaryTree obj;
+	while (true) {
+		cout << "\nMenu" << endl;
+		cout << "1. Implement insert operation" << endl;
+		cout << "2. Perform inorder traversal" << endl;
+		cout << "3. Perform preorder traversal" << endl;
+		cout << "4. Perform postorder traversal" << endl;
+		cout << "5. Exit" << endl;
+		cout << "\nEnter your choice (1-5)" << endl;
+
+		char ch;
+		cin >> ch;
+		cout << endl;
+
+		switch (ch) {
+		case '1': {
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			obj.insert(word);
+			break;
+		}
+		case '2': {
+			obj.inorder(obj.ROOT);
+			break;
+		}
+		case '3': {
+			obj.preorder(obj.ROOT);
+			break;
+		}
+		case '4': {
+			obj.postorder(obj.ROOT);
+			break;
+		}
+		case '5':
+			return 0;
+		default: {
+			cout << "Invalid option" << endl;
+			break;
+		}
+		}
+	}
+}
